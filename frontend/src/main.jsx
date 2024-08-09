426592f81f93160d8d82a56f7e217a812e136b14
Shailen Naidoo <naidooshailen648@gmail.com>
2024-08-09 16:12:02 +0200

    index.css -> main.css
    * Made more sense to rename it to this as there also exists the main.jsx file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
-import './index.css'
+import './main.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
