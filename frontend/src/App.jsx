98496688cfe67c8328ee932375b92ea24d7840e9
Shailen Naidoo <naidooshailen648@gmail.com>
2024-08-09 15:55:45 +0200

    I have installed the necessary dependencies for this React-based Vite setup.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <label htmlFor="">Username</label>
+      <input name="Username" type="text" />
+      <label htmlFor="">Password</label>
+      <input name="Password" type="password"/>
+      <button>Submit</button>
+    </>
+  )
+}
+
+export default App
