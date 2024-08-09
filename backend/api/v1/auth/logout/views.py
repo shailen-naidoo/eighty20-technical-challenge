0344e76706c43676b39d421a6a8a700a5c2e6245
Shailen Naidoo <naidooshailen648@gmail.com>
2024-08-09 13:20:58 +0200

    api/v1/auth/logout/views.py
    * Moved the logout endpoint from the root of the project to this file to make it cleaner to understand the URL paths
    
    app/urls.py
    
    * Migrated the logout endpoint to its own folder

---
+++ backend/api/v1/auth/logout/views.py
@@ -0,0 +1,13 @@
+from rest_framework import status
+from django.contrib.auth import logout
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class LogoutView(APIView):
+    def get(self, request):
+        logout(request)
+        return Response(
+            {'message': 'You have been logged out successfully'},
+            status=status.HTTP_200_OK
+        )
--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,19 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from rest_framework import status
-from django.contrib.auth import logout
-from rest_framework.response import Response
-from rest_framework.views import APIView
 from api.v1.auth.login.views import LoginView
-
-
-class LogoutView(APIView):
-    def get(self, request):
-        logout(request)
-        return Response(
-            {'message': 'You have been logged out successfully'},
-            status=status.HTTP_200_OK
-        )
+from api.v1.auth.logout.views import LogoutView
 
 
 urlpatterns = [
