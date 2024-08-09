41f918cfc7b049e1d0974232a1c60a78685fafda
Shailen Naidoo <naidooshailen648@gmail.com>
2024-08-09 15:30:22 +0200

    dev.sh
    * Created helper shell commands to setup the environment, create a superuser and to run the server

--- backend/dev.sh
+++ backend/dev.sh
@@ -1,4 +1,41 @@
 PROJECT_VIRTUALENV_NAME="eighty20-technical-challenge"
 
-pyenv activate $PROJECT_VIRTUALENV_NAME
-echo "Python project version: $(pyenv local)"
+if command -v pyenv > /dev/null 2>&1; then
+  pyenv activate $PROJECT_VIRTUALENV_NAME
+  echo "Python project version: $(pyenv local)"
+fi
+
+function setup_environment() {
+  ### SETUP PYENV & VIRTUALENV
+
+  rm -rf "/Users/$(whoami)/.pyenv"
+  curl https://pyenv.run | bash # Install pyenv
+  pyenv install # Use pyenv to install the Python version found in .python-version
+  pyenv virtualenv "$(cat .python-version)" $PROJECT_VIRTUALENV_NAME # Create the virutalenv for the project using pyenv
+  pyenv activate $PROJECT_VIRTUALENV_NAME
+
+  ### SETUP POERTY PACKAGE MANAGER & INSTALL PACKAGES
+
+  curl -sSL https://install.python-poetry.org | python3 - # Install poetry python package manager
+  poetry config virtualenvs.create false # Prevent poetry from creating a virtualenv as I want to use pyenv for that.
+  poetry install --no-root # Install the project dependencies
+
+  ### SETUP DJANGO
+
+  python manage.py makemigrations
+  python manage.py migrate
+}
+
+function create_django_superuser() {
+  python manage.py createsuperuser
+}
+
+function run_django_server() {
+  python manage.py runserver
+}
+
+echo ""
+echo "setup_environment - Run this command to setup everything needed to run the Django backend"
+echo "create_django_superuser - Run this command to create a Django superuser"
+echo "run_django_server - Run this command to run the Django server"
+echo ""
